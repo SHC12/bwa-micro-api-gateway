b177b57f2ae6113b014b328112c1b7be8293dd0b
shc12 <superhumancrewid12@gmail.com>
2022-11-14 03:40:11 +0700

    fix: server

---
+++ routes/orderPayments.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const orderPaymentHandler = require('./handler/order-payments');
+
+
+
+router.get('/',orderPaymentHandler.getOrders);
+
+
+
+module.exports = router;
